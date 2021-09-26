77a1ca08b59ce580c65b80c7cfae9ab5a37b2693
mayankv1992 <mayankv1992@gmail.com>
2021-09-27 00:17:03 +1000

    Initial check-in of module LongestSubSequence

---
+++ LongestSubSequence/LongestSubSeqAPI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LongestSubSequence
+{
+    public class LongestSubSeqAPI
+    {
+        public static string LongestSubSeq(string inpSeq)
+        {
+            string[] arrSeq = inpSeq.Split(' ');
+            string outSeq = arrSeq[0];
+            int maxLength = 1;
+            string tempSeq = arrSeq[0];
+
+            for (int i = 0; i < arrSeq.Length - 1; i++)
+            {
+                if (int.Parse(arrSeq[i + 1]) > int.Parse(arrSeq[i]))
+                {
+                    if (tempSeq.Length == 0)
+                    {
+                        tempSeq = arrSeq[i];
+                    }
+                    tempSeq = tempSeq + ' ' + arrSeq[i + 1];
+                    maxLength += 1;
+                }
+                else
+                {
+                    if (outSeq.Split(' ').Length < tempSeq.Split(' ').Length)
+                    {
+                        outSeq = tempSeq;
+                    }
+                    tempSeq = string.Empty;
+                    maxLength = 1;
+
+                }
+            }
+            if (outSeq.Split(' ').Length < tempSeq.Split(' ').Length)
+            {
+                outSeq = tempSeq;
+            }
+            return outSeq;
+        }
+    }
+}
